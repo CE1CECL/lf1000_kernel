9032228dfdc085e035ac9fbdab91123df6b9495f
ChrisEric1 CECL <christopherericlentocha@gmail.com>
2025-06-16 13:04:27 -0400

    Update printk.c

--- kernel/printk.c
+++ kernel/printk.c
@@ -54,7 +54,7 @@ extern void printascii(char *);
 
 /* We show everything that is MORE important than this.. */
 #define MINIMUM_CONSOLE_LOGLEVEL 0 /* Minimum loglevel we let people use */
-#define DEFAULT_CONSOLE_LOGLEVEL 0 /* anything MORE serious than KERN_EMERG */
+#define DEFAULT_CONSOLE_LOGLEVEL 7 /* anything MORE serious than KERN_DEBUG */
 
 DECLARE_WAIT_QUEUE_HEAD(log_wait);
 
